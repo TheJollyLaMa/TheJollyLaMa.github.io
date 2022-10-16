977e856ad9366070cfefed44c48c9b89dbba7633
TheJollyLaMa <j@caffeinelamanna.com>
2022-10-16 07:46:25 -0400

    added angularjs skeleton

--- js/JollysCheatSheet.js
+++ js/JollysCheatSheet.js
@@ -1,1 +1,13 @@
-var app = angular.module('JollysCheatSheet', []);
+var app = angular.module('JollysCheatSheet', ["ngoute"]);
+app.config([
+    "$routeProvider",
+    function($routeProvider) {
+      $routeProvider
+  
+      .when("/", {controller: "MainController", templateUrl: "index.html"})
+      .when("/html", {controller: "MainController", templateUrl: "pages/html.html"})
+      .when("/css", {controller: "MainController", templateUrl: "pages/css.html"})
+
+      .otherwise({redirectTo: "/"})
+    }
+  ]);
