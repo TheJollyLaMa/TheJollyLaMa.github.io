4271c82f9cab1a7f9556baaf6c83f2fe0798a1a4
TheJollyLaMa <j@caffeinelamanna.com>
2022-10-16 08:33:10 -0400

    git clean up after angularjs init

--- js/JollysCheatSheet.js
+++ js/JollysCheatSheet.js
@@ -7,6 +7,11 @@ app.config([
       .when("/", {controller: "MainController", templateUrl: "pages/home.html"})
       .when("/html", {controller: "MainController", templateUrl: "pages/html.html"})
       .when("/css", {controller: "MainController", templateUrl: "pages/css.html"})
+      .when("/js", {controller: "MainController", templateUrl: "pages/js.html"})
+      .when("/fullstack", {controller: "MainController", templateUrl: "pages/fullstack.html"})
+      .when("/tests", {controller: "MainController", templateUrl: "pages/tests.html"})
+      .when("/hosting", {controller: "MainController", templateUrl: "pages/hosting.html"})
+      .when("/git", {controller: "MainController", templateUrl: "pages/git.html"})
 
       .otherwise({redirectTo: "/"})
     }
