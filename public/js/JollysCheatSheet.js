1a3e4c6eb01d18332db8db69d0448999dfb81305
TheJollyLaMa <j@caffeinelamanna.com>
2022-10-16 23:26:13 -0400

    first commit after local dev setup

---
+++ public/js/JollysCheatSheet.js
@@ -0,0 +1,19 @@
+var app = angular.module('JollysCheatSheet', ["ngRoute"]);
+app.config([
+    "$routeProvider",
+    function($routeProvider) {
+      $routeProvider
+  
+      .when("/home", {controller: "MainController", templateUrl: "pages/home.html"})
+      .when("/html", {controller: "MainController", templateUrl: "pages/html.html"})
+      .when("/css", {controller: "MainController", templateUrl: "pages/css.html"})
+      .when("/js", {controller: "MainController", templateUrl: "pages/js.html"})
+      .when("/fullstack", {controller: "MainController", templateUrl: "pages/fullstack.html"})
+      .when("/tests", {controller: "MainController", templateUrl: "pages/tests.html"})
+      .when("/hosting", {controller: "MainController", templateUrl: "pages/hosting.html"})
+      .when("/git", {controller: "MainController", templateUrl: "pages/git.html"})
+      .when("/specialty", {controller: "MainController", templateUrl: "pages/specialprojects.html"})
+
+      .otherwise({redirectTo: "/"})
+    }
+  ]);
