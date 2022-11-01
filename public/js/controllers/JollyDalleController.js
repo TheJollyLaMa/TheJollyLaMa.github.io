26363c0595ad15869fab14e71a9b1c0044c40390
TheJollyLaMa <j@caffeinelamanna.com>
2022-11-01 19:47:25 -0400

    added dalle refresh instructions

--- public/js/controllers/JollyDalleController.js
+++ public/js/controllers/JollyDalleController.js
@@ -1,3 +1,3 @@
 app.controller('JollyDalleController', ['$scope', function($scope) {
-    $scope.title = "Dalle-2 Generated Images";
+    $scope.title = "Dalle-2 Generated Images (refresh for new random image)";
 }]);
