089daed1b98b727d81743b9d7755dc3a7e982aba
TheJollyLaMa <j@caffeinelamanna.com>
2022-12-09 21:14:54 -0500

    adding birthday card

--- public/js/controllers/JollyDalleController.js
+++ public/js/controllers/JollyDalleController.js
@@ -1,3 +1,6 @@
 app.controller('JollyDalleController', ['$scope', function($scope) {
     $scope.title = "Dalle-2 Generated Images (refresh for new random image)";
+    $scope.mandelblum = {
+        "url": "https://decentagency.space/public/img/mandel_toroid_clear.gif"
+    }
 }]);
---
+++ public/js/controllers/MandelCubeController.js
@@ -0,0 +1,6 @@
+app.controller('MandelCubeController', ['$scope', function($scope) {
+    $scope.title = "Blender Art (refresh for new random image)";
+    $scope.blender_art_urls = {
+        "mandel_toroid_clear": "https://decentagency.space/public/img/mandel_toroid_clear.gif"
+    }
+}]);
