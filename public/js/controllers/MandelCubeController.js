9e47f57093862ad182e558001f8649c3629ea8cc
TheJollyLaMa <j@caffeinelamanna.com>
2022-12-13 08:19:13 -0500

    adding mandelCube

--- public/js/controllers/MandelCubeController.js
+++ public/js/controllers/MandelCubeController.js
@@ -1,6 +1,14 @@
 app.controller('MandelCubeController', ['$scope', function($scope) {
     $scope.title = "Blender Art (refresh for new random image)";
     $scope.blender_art_urls = {
-        "mandel_toroid_clear": "https://decentagency.space/public/img/mandel_toroid_clear.gif"
+        "mandelblum_clear": "https://decentagency.space/public/img/mandelblum_clear.gif",
+        "mandel_toroid_clear": "https://decentagency.space/public/img/mandel_toroid_clear.gif",
+        "mandelblum_clear_royal": "https://decentagency.space/public/img/mandelblum_clear_royal.gif"
+    };
+    $scope.randomMandelBlum = function(urls) {
+        var keys = Object.keys(urls);
+        var random_key = keys[Math.floor(Math.random() * keys.length)];
+        var random_url = urls[random_key];
+        return random_url;
     }
 }]);
--- public/js/directives/mandelCube.js
+++ public/js/directives/mandelCube.js
@@ -5,67 +5,6 @@ app.directive('mandelCube', function() {
             data: '='
         },
         controller: "MandelCubeController",
-        templateUrl: 'js/directives/mandelCube.html',
-        link: function(scope, elem, attrs, data ) {
-            // three.js scene
-            let scene, camera, renderer, cube;
-            function init () {
-                // init scene
-                scene = new THREE.Scene();
-                // position scene in center of screen
-                scene.position.set(0, 1.25, 0);
-                // init camera
-                camera = new THREE.PerspectiveCamera(
-                    75, // Field of view 
-                    window.innerWidth / window.innerHeight , // Aspect ratio (width / height)
-                    0.1, // near plane (objects closer than this will not be rendered)
-                    1000 // far plane not displayed because it's too far
-                );
-
-                // init the renderer
-                renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true});
-
-                // set the size of the renderer
-                renderer.setSize(window.innerWidth, window.innerHeight, false);
-
-                // add the renderer to the DOM
-                document.getElementById("dalle_cube").appendChild(renderer.domElement);
-
-                // create a cube
-                const geometry = new THREE.BoxGeometry(2, 2, 2);
-                // const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-                // set random texture file from the textures folder    
-                // const texture = new THREE.TextureLoader().load('img/textures/' + Math.floor(Math.random() * 19) + '.png');
-                const texture = new THREE.TextureLoader().load('https://decentagency.space/public/img/mandel_toroid_clear.gif');
-                // const texture = new THREE.TextureLoader().load('textures/19.gif');
-                
-                const material = new THREE.MeshBasicMaterial({ map: texture });
-
-                cube = new THREE.Mesh(geometry, material);
-                scene.add(cube);
-
-                // set the camera position
-                camera.position.z = 5;
-
-            }
-
-            function animate() {
-                requestAnimationFrame(animate);
-                cube.rotation.x += 0.01;
-                cube.rotation.y += 0.01;
-                renderer.render(scene, camera);
-            }
-
-            function onWindowResize() {
-                camera.aspect = window.innerWidth / window.innerHeight;
-                camera.updateProjectionMatrix();
-                renderer.setSize(window.innerWidth, window.innerHeight);
-            }
-
-            window.addEventListener('resize', onWindowResize, false);
-
-            init();
-            animate();
-        }
+        templateUrl: 'js/directives/mandelCube.html'
     }
 });
