d00eb89a6c5b7221c200ec5eba0e127c6a74b4a6
TheJollyLaMa <j@caffeinelamanna.com>
2022-12-21 06:10:02 -0500

    adding DecentArch

--- public/js/controllers/FoundMoundsController.js
+++ public/js/controllers/FoundMoundsController.js
@@ -1,3 +1,12 @@
 app.controller('FoundMoundsController', ['$scope', function($scope) {
 
+    // submit geolocation form
+
+    // submit trip ticket form
+
+    // submit story form
+
+
+    // service factories to show connected wallet's interactions with global Archeological record
+
 }]);
